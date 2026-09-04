c4c5a01757caade424635df8955b0529feb04f86
Cristopher_Lopez <cr.lopezr@duocuc.cl>
2026-09-03 22:58:44 -0400

    Error de prueba Controller

--- src/main/java/com/pokemon/repository/PokemonRepository.java
+++ src/main/java/com/pokemon/repository/PokemonRepository.java
@@ -4,6 +4,8 @@ import com.pokemon.entity.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Repositorio para la entidad Pokemon.
  *
@@ -18,4 +20,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
+    Optional<Pokemon> findByNameIgnoreCase(String name);
 }
